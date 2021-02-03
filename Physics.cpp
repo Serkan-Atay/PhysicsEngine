d652baf3116259dcdab5828d017e67dea64b636d
Serkan <Serkan.Atay93@Gmail.Com>
2021-02-03 21:57:19 +0100

    URLs.

--- Physics.cpp
+++ Physics.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
-// Axis Aligned Bounding Boxes
+//Axis Aligned Bounding Boxes
 struct AABB {
     Vec2 min;
     Vec2 max;
 };
 
-// Circle formula
+//Circle formula
 struct Circle {
     float radius;
     Vec position;
 };
 
-// Test
+//Test
+
+//https://github.com/danistefanovic/build-your-own-x#build-your-own-physics-engine
+
+//https://gamedevelopment.tutsplus.com/tutorials/how-to-create-a-custom-2d-physics-engine-the-basics-and-impulse-resolution--gamedev-6331
