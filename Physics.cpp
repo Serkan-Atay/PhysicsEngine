b9ce1a05a95766beb327fabfb45375f425dd1dff
Serkan <Serkan.Atay93@Gmail.com>
2021-02-03 21:33:49 +0100

    Initializing.

---
+++ Physics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Axis Aligned Bounding Boxes
+struct AABB {
+    Vec2 min;
+    Vec2 max;
+};
+
+// Circle formula
+struct Circle {
+    float radius;
+    Vec position;
+};
