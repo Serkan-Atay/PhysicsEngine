26d48116f2c51f1325191040314faa03b8bb4aba
Serkan <Serkan.Atay93@Gmail.Com>
2021-02-04 17:54:18 +0100

    Changed the file name.

---
+++ Engine.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//Axis Aligned Bounding Boxes
+struct AABB {
+    Vec2 min;
+    Vec2 max;
+};
+
+//Circle formula
+struct Circle {
+    float radius;
+    Vec position;
+};
+
+//Test
+
+//https://github.com/danistefanovic/build-your-own-x#build-your-own-physics-engine
+
+//https://gamedevelopment.tutsplus.com/tutorials/how-to-create-a-custom-2d-physics-engine-the-basics-and-impulse-resolution--gamedev-6331
